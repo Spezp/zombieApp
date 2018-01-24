72bc547b984841e9f9fa37448c9378cf50361c6f
vagrant <spencer.greff@gmail.com>
2018-01-24 23:28:46 +0000

    Made Update form functional. Made some small styling changes for sanity

--- express_server.js
+++ express_server.js
@@ -49,6 +49,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   console.log(templateVars.longURL);

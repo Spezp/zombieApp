a647960ae6434d0f47aad7235f79ca0e908587b1
vagrant <spencer.greff@gmail.com>
2018-01-25 21:21:13 +0000

    Added Registration page and forms. GET registration in app.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cookieParser());
@@ -13,6 +14,8 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 // Generate Random String and returns a six digit alphanumeric string.
 
 function generateRandomString() {
@@ -26,27 +29,28 @@ function generateRandomString() {
 
 console.log(generateRandomString());
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
-
 app.get("/urls", (req, res) => {
-
   let templateVars = { urls: urlDatabase, username: req.cookies.username };
   console.log(req.cookies.username);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
+  res.render("urls_new", templateVars);
 });
 
+app.get("/urls/register", (req, res) => {
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
+  res.render("urls_register", templateVars);
+});
+
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
+  // debug statement to see POST parameters
   let newRandString = generateRandomString();
   urlDatabase[newRandString] = req.body.longURL;
-  console.log(urlDatabase);
-  res.redirect(`/urls/${newRandString}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${newRandString}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -54,29 +58,39 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+// For updateding the URL provided by user while keeping the same TinyApp
 app.post("/urls/:id/update", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
 });
 
+// Logs in user
 app.post("/urls/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 
+// Clears user login cookie
 app.post("/urls/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
 });
 
+// Handles Registration
+app.post("/urls/logout", (req, res) => {
+
+});
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.params.id],
+    urls: urlDatabase,
+    username: req.cookies.username
   };
   console.log(templateVars.longURL);
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
